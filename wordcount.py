59bfc767321a476165a1d2f02aa1a7c1650ab206
Vincenzo Abete <abetevincenzo@gmail.com>
2021-03-01 19:20:18 +0000

    Words are lowercase and split on dashes now. Ordered the table

--- wordcount.py
+++ wordcount.py
@@ -1,7 +1,16 @@
 import sys
-from operator import add
 
 from pyspark.sql import SparkSession, DataFrame, functions as F, Window
+
+def lower_and_clean_string(string_to_edit):
+        # define a list of all punctuation needed to be removed
+        punctuation_to_remove='!"#$%&\'()*+,./:;<=>?@[\\]^_`{|}~'
+        # Replace dashes with spaces and make lowercase 
+        lowercased_string = string_to_edit.replace('-',' ').lower()
+        # loop over each mark to remove and subsitute it with empty space in string
+        for character in punctuation_to_remove:
+            lowercased_string = lowercased_string.replace(character, '')
+        return lowercased_string
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -13,22 +22,36 @@ if __name__ == "__main__":
         .appName("PythonWordCount")\
         .getOrCreate()
 
+    # read the text from the file, and create an RDD of lines
     lines = spark.read.text(sys.argv[1]).rdd.map(lambda r: r[0])
+    
+    # remove puntuaction marks, separate dashed words and make them lowercase
+    lines = lines.map(lower_and_clean_string)
+
+    # first we generate a flat map of single words
+    # then we add a 1 counter to each word
+    # then we "group by" word and sum the 1 added before for each entry (word, X)
     counts = lines.flatMap(lambda words: words.split(' ')) \
                   .map(lambda word: (word, 1)) \
-                  .reduceByKey(lambda a, b: a+b) \
+                  .reduceByKey(lambda a, b: a + b) \
 
+    # collect (?)
     output = counts.collect()
 
-    df = spark.createDataFrame(output, ('Word', 'Count'))
+    # create a spark dataframe (table)
+    df = spark.createDataFrame(output, ("Word", "Count"))
 
-    # Add Rank (+1 index)
-    w = Window().orderBy("count")
-    df = df.withColumn('Rank', F.row_number().over(w))
+    # Add Rank (row index) column
+    w = Window().orderBy(F.col("count").desc(),F.col("Word").asc())
+    df = df.withColumn("Rank", F.row_number().over(w))
 
-    df.show()
+    # display the table .show(truncate=False) not working...
+    df.orderBy(F.col("count").desc(),F.col("Word").asc()).show(100)
 
+    # temp. Show each word
     for (word, count) in output:
         print("%s: %i" % (word, count))
+
+    print("total {}".format(df.count()))
 
     spark.stop()
