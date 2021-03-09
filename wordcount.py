182c7c1d4fe6ef5db097a2a6ff20ad518cfa6fc8
Vincenzo Abete <abetevincenzo@gmail.com>
2021-03-09 18:32:26 +0000

    Using pandas dataframe for easy sorting...

--- wordcount.py
+++ wordcount.py
@@ -1,17 +1,16 @@
 import sys
 import math
+import pandas as pd
 
 from pyspark.sql import SparkSession, DataFrame, functions as F, Window
 
 def lower_and_clean_string(string_to_edit):
         # define a list of all punctuation needed to be removed
-        punctuation_to_remove='!"#$%&\'()*+,./:;<=>?@[\\]^_`{|}~'
-        # Replace dashes with spaces and make lowercase 
-        lowercased_string = string_to_edit.replace('-',' ').lower()
+        punctuation_to_remove='!"#$%&\'()*+-,./:;<=>?@[\\]^_`{|}~'
         # loop over each mark to remove and subsitute it with empty space in string
         for character in punctuation_to_remove:
-            lowercased_string = lowercased_string.replace(character, '')
-        return lowercased_string
+            string_to_edit = string_to_edit.replace(character, '')
+        return string_to_edit
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -36,18 +35,48 @@ if __name__ == "__main__":
                   .map(lambda word: (word, 1)) \
                   .reduceByKey(lambda a, b: a + b) \
 
-    # frequency_and_word_pairs = word_and_frequency_pairs.map(lambda x: (x[1],x[0]))
-
-    word_and_frequency_pairs = word_and_frequency_pairs.sortBy(lambda a: a[1], ascending=False)
-
-    print(word_and_frequency_pairs.take(100))
-
     columns = ["Word","Frequency"]
 
     df = word_and_frequency_pairs.toDF(columns)
 
-    df.show(100)
+    pandasDataframe = df.toPandas()
+    pandasDataframeSorted = pandasDataframe.sort_values(by=['Frequency', 'Word'], ascending=[False, True])
+    pandasDataframeSorted.reset_index(drop=True, inplace=True)
+    pandasDataframeSorted.index += 1
 
+    print(pandasDataframeSorted)
+
+    pySparkDataFrame = spark.createDataFrame(pandasDataframeSorted)
+
+    # Add Rank (row index) column
+    w = Window().orderBy(F.col("Frequency").desc(),F.col("Word").asc())
+    pySparkDataFrame = pySparkDataFrame.withColumn("Rank", F.row_number().over(w))   
+    
+    pySparkDataFrame.show(100)
+
+    # # calculate 5 percent of total records
+    five_percent = math.ceil((pySparkDataFrame.count())*(5/100))
+    
+    popular_words = pySparkDataFrame.filter(pySparkDataFrame.Rank.between(1,five_percent))
+    
+    # how to get the records ranked between 47.5% and 52.5% ???
+    common_words = pySparkDataFrame.filter(pySparkDataFrame.Rank.between(21,24))
+    
+    # how to get the last 5% of records???
+    rare_words = pySparkDataFrame.filter(pySparkDataFrame.Rank.between(40,43))
+    
+    print('Popular words')
+    popular_words.show()
+    
+    print('Common words')
+    common_words.show()
+    
+    print('Rare words')
+    rare_words.show()
+    
+    # stop the Spark Session
+    spark.stop()
+ 
 
     # # collect (?)
     # output = counts.collect()
@@ -71,5 +100,3 @@ if __name__ == "__main__":
     #     print("%s: %i" % (word, count))
 
     # print("total {}".format(df.count()))
-
-    spark.stop()
